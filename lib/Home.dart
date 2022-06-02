0f78cca0a983868ffedca088514c22bcaec7ef0c
sudirman-nur-putra <sudirman.110snp@gmail.com>
2022-06-02 16:00:25 +0700

    update rangka

---
+++ lib/Home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'RincianFakultas.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
+        InkWell(
+          child: Container(
+              decoration: BoxDecoration(border: Border.all()),
+              padding: EdgeInsets.all(14),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  new Text("FPMIPA", textAlign: TextAlign.center),
+                  Text(
+                      "Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam",
+                      textAlign: TextAlign.left),
+                ],
+              )),
+          onTap: () {
+            //gunakan navigator untuk panggil RincianFakultas
+          },
+        ),
+        Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                new Text("FPIPS", textAlign: TextAlign.center),
+                Text("Fakultas Pendidikan Ilmu Pengetahuan Sosial",
+                    textAlign: TextAlign.left),
+              ],
+            )),
+      ]),
+    );
+  }
+}
