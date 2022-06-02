35cbb21ee96832b6fbb5655bc7570c8abe7032a0
sudirman-nur-putra <72235549+sudirman-nur-putra@users.noreply.github.com>
2022-06-03 03:32:29 +0700

    Update Fakultas.dart

--- lib/Fakultas.dart
+++ lib/Fakultas.dart
@@ -6,36 +6,37 @@ class Fakultas extends StatelessWidget {
   const Fakultas({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
-        InkWell(
-          child: Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  new Text("FPMIPA", textAlign: TextAlign.center),
-                  Text(
-                      "Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam",
-                      textAlign: TextAlign.left),
-                ],
-              )),
-          onTap: () {
-            //gunakan navigator untuk panggil RincianFakultas
-          },
+    return Scaffold(
+      body: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
+        Text(
+          "Fasilitas Fakultas",
+          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         ),
         Container(
-            decoration: BoxDecoration(border: Border.all()),
-            padding: EdgeInsets.all(14),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                new Text("FPIPS", textAlign: TextAlign.center),
-                Text("Fakultas Pendidikan Ilmu Pengetahuan Sosial",
-                    textAlign: TextAlign.left),
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Colors.grey.shade200,
+                    offset: Offset(2, 4),
+                    blurRadius: 5,
+                    spreadRadius: 2)
               ],
-            )),
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(255, 151, 151, 151),
+                    Color.fromARGB(255, 199, 199, 199)
+                  ])),
+          child: Column(
+            children: <Widget>[
+              Text("Selamat Datang"),
+            ],
+          ),
+        ),
       ]),
     );
   }
