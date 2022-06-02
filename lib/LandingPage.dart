b8d173d7820a6b34bfee580da35a713aa01ca4fd
sudirman-nur-putra <72235549+sudirman-nur-putra@users.noreply.github.com>
2022-06-03 03:31:51 +0700

    Update LandingPage.dart

--- lib/LandingPage.dart
+++ lib/LandingPage.dart
@@ -6,37 +6,38 @@ class LandingPage extends StatelessWidget {
   const LandingPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
-        InkWell(
-          child: Container(
-              decoration: BoxDecoration(border: Border.all()),
-              padding: EdgeInsets.all(14),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  new Text("FPMIPA", textAlign: TextAlign.center),
-                  Text(
-                      "Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam",
-                      textAlign: TextAlign.left),
-                ],
-              )),
-          onTap: () {
-            //gunakan navigator untuk panggil RincianFakultas
-          },
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('background.png'), fit: BoxFit.fill),
+          borderRadius: BorderRadius.all(Radius.circular(5)),
         ),
-        Container(
-            decoration: BoxDecoration(border: Border.all()),
-            padding: EdgeInsets.all(14),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                new Text("FPIPS", textAlign: TextAlign.center),
-                Text("Fakultas Pendidikan Ilmu Pengetahuan Sosial",
-                    textAlign: TextAlign.left),
-              ],
-            )),
-      ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Center(
+              child: ListView(shrinkWrap: true, children: [
+                new Image.asset(
+                  "logoupi.png",
+                  height: 200.0,
+                  width: 200.0,
+                ),
+                Text(""),
+                Text(""),
+                new Image.asset(
+                  "upi_logo_smallcopy.png",
+                  height: 70.0,
+                  width: 170.0,
+                ),
+              ]),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
